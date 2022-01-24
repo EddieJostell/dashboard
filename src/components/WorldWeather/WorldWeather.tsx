5d62b2ac8ce04481304e41a5626b04ce26e3475a
Eddie <eddie.jostell@gmail.com>
2022-01-24 23:56:09 +0100

    Global weather incoming

--- src/components/WorldWeather/WorldWeather.tsx
+++ src/components/WorldWeather/WorldWeather.tsx
@@ -1,32 +1,58 @@
-import React, { useState } from 'react';
-import { countries, ICountries } from '../../utils/country_capitals';
+import React, { useEffect, useState } from 'react';
+import { countries } from '../../utils/country_capitals';
 
-interface IWeatherState {}
+interface IWeatherState {
+  capitalName: string;
+  weatherData: string[];
+}
 
 const WorldWeather = () => {
-  const [capital, setCapital] = useState<IWeatherState>({
-    capitalName: 'Stockholm',
+  const [weatherInfo, setWeatherInfo] = useState<IWeatherState>({
+    capitalName: '',
+    weatherData: [],
   });
 
   const changeCity = (event: any) => {
-    setCapital({ ...capital, capitalName: event?.target.value });
+    setWeatherInfo({ ...weatherInfo, capitalName: event?.target.value });
   };
 
+  useEffect(() => {
+    let weather = `https://api.openweathermap.org/data/2.5/weather?q=${weatherInfo.capitalName}&units=metric&APPID=47e6a9e5eec596e801f83d4ab090443b`;
+    fetch(weather)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log('data', data);
+      })
+      .catch(function (error) {
+        console.log('Request failed', error);
+      });
+  }, [weatherInfo.capitalName]);
+
   const dropdownOptions = () => {
-    return countries.map((opt, key) => (
+    let sortedList = countries.sort(function (a, b) {
+      return a.CapitalName.localeCompare(b.CapitalName);
+    });
+
+    return sortedList.map((opt, key) => (
       <option key={key} value={opt.CapitalName}>
         {opt.CapitalName} - {opt.CountryName}
       </option>
     ));
   };
+
+  /*  const infoFromApi = (response: any) => {
+    console.log(response);
+    return <div></div>;
+  }; */
+
   return (
     <div>
       <label>
         <h3>Cities</h3>
       </label>
-      <select className="cod-select" onChange={changeCity}>
-        {dropdownOptions()}
-      </select>
+      <select onChange={changeCity}>{dropdownOptions()}</select>
     </div>
   );
 };
