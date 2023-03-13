d5428f44cf372bb20939e1ae80b0924a0bd7e01b
Eddie <edward.kelder.jostell@skandia.se>
2023-03-13 15:12:02 +0100

    Upgraded to React 18 + simple animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,30 @@ import './App.scss';
 import Container from './components/Container/Container';
 import WorldWeather from './components/WorldWeather/WorldWeather';
 import Clock from './components/Clock/Clock';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="title">Dashboard</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <motion.h1
+          initial={{
+            opacity: 0,
+            scale: 1,
+          }}
+          animate={{
+            opacity: 1,
+            scale: [1, 1.5, 1],
+            transition: { duration: 1.5, delay: 0.5 },
+          }}
+          className='title'
+        >
+          Dashboard
+        </motion.h1>
       </header>
       <Container>
-        <div className="Dashboard">
+        <div className='Dashboard'>
           <WorldWeather />
           <Clock />
         </div>
